07e14ac596e74844eacaa01b76e1f5fddef40c03
Per Liedman <per@liedman.net>
2015-10-19 23:53:04 +0200

    Better command line args

--- cli.js
+++ cli.js
@@ -3,11 +3,23 @@
 var osm2obj = require('./'),
     fs = require('fs'),
     TileSet = require('node-hgt').TileSet,
-    osmdata = JSON.parse(fs.readFileSync(process.argv[2])),
-    elevationProvider = new TileSet(process.argv[3]);
+    proj4 = require('proj4'),
+    argv = require('minimist')(process.argv.slice(2)),
+    osmdata = JSON.parse(fs.readFileSync(argv._[0])),
+    elevationProvider,
+    options;
+
+if (argv['elevation-data'] || argv.e) {
+    elevationProvider = new TileSet(argv['elevation-data'] || argv.e);
+}
+
+options = {
+    ground: argv.ground || argv.g || false,
+    projection: argv.projection ? proj4(proj4.WGS84, argv.projection) : undefined
+};
 
 osm2obj(osmdata, process.stdout, elevationProvider, function(err) {
     if (err) {
         process.stderr.write(err + '\n\n');
     }
-});
+}, options);
